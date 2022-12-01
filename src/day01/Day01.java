3f62dbd125ecd4d2a76ca60f67cb7e83c050de82
Verda Bursal <verda.bursal@gmail.com>
2022-11-30 22:10:52 -0800

    clean up day01

--- src/day01/Day01.java
+++ src/day01/Day01.java
@@ -11,39 +11,28 @@ public class Day01 {
 	String filepath = "src/day01/day01data.txt";
 
 	public int solvePuzzle1() {
-		List<Integer> calorieCounts = new ArrayList<>();
-
-		// read the txt file and populate the array
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(filepath));
-			String currentLine;
-			int currentCalorieCount = 0;
-			while ((currentLine = br.readLine()) != null) {
-				if (!currentLine.equals("")) {
-					currentCalorieCount += Integer.parseInt(currentLine);
-				} else {
-					calorieCounts.add(currentCalorieCount);
-					currentCalorieCount = 0;
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		int currentMax = 0;
+		List<Integer> calorieCounts = findCalorieCounts();
+		int maxCalories = 0;
 		for (int c : calorieCounts) {
-			if (c > currentMax) {
-				currentMax = c;
+			if (c > maxCalories) {
+				maxCalories = c;
 			}
 		}
 
-		return currentMax;
+		return maxCalories;
 	}
 
 	public int solvePuzzle2() {
+		List<Integer> calorieCounts = findCalorieCounts();
+		Collections.sort(calorieCounts);
+		int n = calorieCounts.size();
+
+		return calorieCounts.get(n - 1) + calorieCounts.get(n - 2) + calorieCounts.get(n - 3);
+	}
+
+	private List<Integer> findCalorieCounts() {
 		List<Integer> calorieCounts = new ArrayList<>();
 
-		// read the txt file and populate the array
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filepath));
 			String currentLine;
@@ -60,8 +49,6 @@ public class Day01 {
 			e.printStackTrace();
 		}
 
-		Collections.sort(calorieCounts);
-		int n = calorieCounts.size();
-		return calorieCounts.get(n - 1) + calorieCounts.get(n - 2) + calorieCounts.get(n - 3);
+		return calorieCounts;
 	}
 }
