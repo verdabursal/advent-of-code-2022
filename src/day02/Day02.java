3f7748cc7089765e419d1c2c1793b4a5f892e987
Verda Bursal <verda.bursal@gmail.com>
2022-12-01 22:05:46 -0800

    clean up day02

--- src/day02/Day02.java
+++ src/day02/Day02.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 
 public class Day02 {
 	String filepath = "src/day02/day02data.txt";
+
+	String abc = "ABC";
+	String xyz = "XYZ";
 	public int solvePuzzle1() {
 		int totalScore = 0;
 
@@ -13,31 +16,16 @@ public class Day02 {
 			BufferedReader br = new BufferedReader(new FileReader(filepath));
 			String currentLine;
 			while ((currentLine = br.readLine()) != null) {
-				char opponent = currentLine.charAt(0);
-				char self = currentLine.charAt(2);
-				int score = 0;
-				if (self == 'X') {
-					score += 1;
-					if (opponent == 'C') {
-						score += 6;
-					} else if (opponent == 'A') {
-						score += 3;
-					}
-				} else if (self == 'Y') {
-					score += 2;
-					if (opponent == 'A') {
-						score += 6;
-					} else if (opponent == 'B') {
-						score += 3;
-					}
-				} else if (self == 'Z') {
+				int opponent = abc.indexOf(currentLine.charAt(0));
+				int self = xyz.indexOf(currentLine.charAt(2));
+				int score = self + 1;
+
+				if (opponent == self) { // draw
 					score += 3;
-					if (opponent == 'B') {
-						score += 6;
-					} else if (opponent == 'C') {
-						score += 3;
-					}
+				} else if ((opponent + 1) % 3 == self % 3) { // win
+					score += 6;
 				}
+
 				totalScore += score;
 			}
 		} catch (IOException e) {
@@ -54,43 +42,16 @@ public class Day02 {
 			BufferedReader br = new BufferedReader(new FileReader(filepath));
 			String currentLine;
 			while ((currentLine = br.readLine()) != null) {
-				char opponent = currentLine.charAt(0);
-				char result = currentLine.charAt(2);
+				int opponent = abc.indexOf(currentLine.charAt(0));
+				int result = xyz.indexOf(currentLine.charAt(2));
 				int score = 0;
 
-				if (opponent == 'A') {
-					if (result == 'X') {
-						// lose -- self=scissors
-						score += 3;
-					} else if (result == 'Y') {
-						// draw -- self=rock
-						score += 3 + 1;
-					} else {
-						// win -- self=paper
-						score += 6 + 2;
-					}
-				} else if (opponent == 'B') {
-					if (result == 'X') {
-						// lose -- self=rock
-						score += 1;
-					} else if (result == 'Y') {
-						// draw -- self=paper
-						score += 3 + 2;
-					} else {
-						// win -- self=scissors
-						score += 6 + 3;
-					}
-				} else if (opponent == 'C') {
-					if (result == 'X') {
-						// lose -- self=paper
-						score += 2;
-					} else if (result == 'Y') {
-						// draw -- self=scissors
-						score += 3 + 3;
-					} else {
-						// win -- self=rock
-						score += 6 + 1;
-					}
+				if (result == 0) { // lose
+					score += (opponent + 2) % 3 + 1;
+				} else if (result == 1) { // draw
+					score += (opponent - 1) % 3 + 5;
+				} else { // win
+					score += (opponent + 1) % 3 + 7;
 				}
 
 				totalScore += score;
