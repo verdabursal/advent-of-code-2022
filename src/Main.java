44d6023785b5976c83a17faeb3fe98ec6823106b
Verda Bursal <verda.bursal@gmail.com>
2022-12-02 21:15:25 -0800

    solve day03

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import day02.Day02;
+import day03.Day03;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(new Day02().solvePuzzle2());
+		System.out.println(new Day03().solvePuzzle2());
 	}
 }
--- src/day03/Day03.java
+++ src/day03/Day03.java
@@ -7,21 +7,74 @@ import java.io.IOException;
 public class Day03 {
 	String filepath = "src/day03/data.txt";
 
+	String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 	public int solvePuzzle1() {
+		int prioritiesSum = 0;
+
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filepath));
 			String currentLine;
 			while ((currentLine = br.readLine()) != null) {
-				//
+				Character shared = null;
+				String firstHalf = currentLine.substring(0, currentLine.length() / 2);
+				String secondHalf = currentLine.substring(currentLine.length() / 2);
+				char[] firstHalfChars = firstHalf.toCharArray();
+
+				for (char c : firstHalfChars) {
+					if (secondHalf.indexOf(c) > -1) {
+						shared = c;
+						break;
+					}
+				}
+
+				if (shared != null) {
+					prioritiesSum += alphabet.indexOf(shared) + 1;
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return 0;
+		return prioritiesSum;
 	}
 
 	public int solvePuzzle2() {
-		return 0;
+		int prioritiesSum = 0;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			String currentLine;
+			int idx = 0;
+			String[] currentTrio = new String[3];
+
+			while ((currentLine = br.readLine()) != null) {
+				currentTrio[idx] = currentLine;
+
+				if (idx == 2) {
+					Character shared = null;
+					char[] firstOfTrioChars = currentTrio[0].toCharArray();
+
+					for (char c : firstOfTrioChars) {
+						if (currentTrio[1].indexOf(c) > -1 && currentTrio[2].indexOf(c) > -1) {
+							shared = c;
+							break;
+						}
+					}
+
+					if (shared != null) {
+						prioritiesSum += alphabet.indexOf(shared) + 1;
+					}
+
+					idx = 0;
+				} else {
+					idx++;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return prioritiesSum;
 	}
 }
