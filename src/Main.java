c8f7a463f1c79ad4ef3d9847c1e265485f228fae
Verda Bursal <verda.bursal@gmail.com>
2022-12-11 21:53:16 -0800

    solve day12, very hacky

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import day11.Day11;
+import day12.Day12;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(new Day11().solvePuzzle2());
+		System.out.println(new Day12().solvePuzzle2());
 	}
 }
--- src/day12/Day12.java
+++ src/day12/Day12.java
@@ -3,25 +3,303 @@ package day12;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Day12 {
 	String filepath = "src/day12/data.txt";
+	String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 	public int solvePuzzle1() {
+		List<List<Integer>> matrix = new ArrayList<>();
+		int[] start = new int[2];
+		int[] end = new int[2];
+
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			int i = 0;
 			String currentLine;
 			while ((currentLine = br.readLine()) != null) {
-				//
+				List<Integer> matrixRow = new ArrayList<>();
+				for (int j = 0; j < currentLine.length(); j++) {
+					switch (currentLine.charAt(j)) {
+						case 'S':
+							start[0] = i;
+							start[1] = j;
+							matrixRow.add(0);
+							break;
+						case 'E':
+							end[0] = i;
+							end[1] = j;
+							matrixRow.add(25);
+							break;
+						default:
+							matrixRow.add(alphabet.indexOf(currentLine.charAt(j)));
+							break;
+					}
+				}
+				matrix.add(matrixRow);
+				i++;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return 0;
+		// transform matrix into a graph
+		Graph graph = new Graph();
+		List<List<Node>> tempNodes = new ArrayList<>();
+		for (int i = 0; i < matrix.size(); i++) {
+			List<Node> tempNodesRow = new ArrayList<>();
+			for (int j = 0; j < matrix.get(0).size(); j++) {
+				Node node = new Node(i + "," + j);
+				tempNodesRow.add(node);
+			}
+			tempNodes.add(tempNodesRow);
+		}
+		Set<Node> nodes = new HashSet<>();
+		for (int i = 0; i < matrix.size(); i++) {
+			for (int j = 0; j < matrix.get(0).size(); j++) {
+				Node node = tempNodes.get(i).get(j);
+				if (i > 0 && matrix.get(i - 1).get(j) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i - 1).get(j), 1);
+				}
+				if (j > 0 && matrix.get(i).get(j - 1) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i).get(j - 1), 1);
+				}
+				if (i < matrix.size() - 1 && matrix.get(i + 1).get(j) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i + 1).get(j), 1);
+				}
+				if (j < matrix.get(0).size() - 1 && matrix.get(i).get(j + 1) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i).get(j + 1), 1);
+				}
+				nodes.add(node);
+			}
+		}
+		graph.setNodes(nodes);
+
+		Graph dijkstra = calculateShortestPathFromSource(graph, tempNodes.get(start[0]).get(start[1]));
+
+		Node endNode = new Node("");
+		// find the end node
+		for (Node n : dijkstra.getNodes()) {
+			if (n.getName().equals(end[0] + "," + end[1])) {
+				endNode = n;
+			}
+		}
+
+		return endNode.getDistance();
 	}
 
 	public int solvePuzzle2() {
-		return 0;
+		List<List<Integer>> matrix = new ArrayList<>();
+		List<int[]> starts = new ArrayList<>();
+		int[] end = new int[2];
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			int i = 0;
+			String currentLine;
+			while ((currentLine = br.readLine()) != null) {
+				List<Integer> matrixRow = new ArrayList<>();
+				for (int j = 0; j < currentLine.length(); j++) {
+					switch (currentLine.charAt(j)) {
+						case 'a':
+							starts.add(new int[]{i, j});
+							matrixRow.add(0);
+							break;
+						case 'E':
+							end[0] = i;
+							end[1] = j;
+							matrixRow.add(25);
+							break;
+						default:
+							matrixRow.add(alphabet.indexOf(currentLine.charAt(j)));
+							break;
+					}
+				}
+				matrix.add(matrixRow);
+				i++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// transform matrix into a graph
+		Graph graph = new Graph();
+		List<List<Node>> tempNodes = new ArrayList<>();
+		for (int i = 0; i < matrix.size(); i++) {
+			List<Node> tempNodesRow = new ArrayList<>();
+			for (int j = 0; j < matrix.get(0).size(); j++) {
+				Node node = new Node(i + "," + j);
+				tempNodesRow.add(node);
+			}
+			tempNodes.add(tempNodesRow);
+		}
+		Set<Node> nodes = new HashSet<>();
+		for (int i = 0; i < matrix.size(); i++) {
+			for (int j = 0; j < matrix.get(0).size(); j++) {
+				Node node = tempNodes.get(i).get(j);
+				if (i > 0 && matrix.get(i - 1).get(j) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i - 1).get(j), 1);
+				}
+				if (j > 0 && matrix.get(i).get(j - 1) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i).get(j - 1), 1);
+				}
+				if (i < matrix.size() - 1 && matrix.get(i + 1).get(j) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i + 1).get(j), 1);
+				}
+				if (j < matrix.get(0).size() - 1 && matrix.get(i).get(j + 1) - matrix.get(i).get(j) <= 1) {
+					node.getAdjacentNodes().put(tempNodes.get(i).get(j + 1), 1);
+				}
+				nodes.add(node);
+			}
+		}
+		graph.setNodes(nodes);
+
+		int shortestDistance = Integer.MAX_VALUE;
+
+		for (int[] start : starts) {
+			Graph dijkstra = calculateShortestPathFromSource(graph, tempNodes.get(start[0]).get(start[1]));
+			Node endNode = new Node("");
+			// find the end node
+			for (Node n : dijkstra.getNodes()) {
+				if (n.getName().equals(end[0] + "," + end[1])) {
+					endNode = n;
+				}
+			}
+			int dist = endNode.getDistance();
+			if (dist < shortestDistance) {
+				shortestDistance = dist;
+			}
+		}
+
+		return shortestDistance;
+	}
+
+	Graph calculateShortestPathFromSource(Graph graph, Node source) {
+		source.setDistance(0);
+
+		Set<Node> settledNodes = new HashSet<>();
+		Set<Node> unsettledNodes = new HashSet<>();
+
+		unsettledNodes.add(source);
+
+		while (unsettledNodes.size() != 0) {
+			Node currentNode = getLowestDistanceNode(unsettledNodes);
+			unsettledNodes.remove(currentNode);
+			for (Map.Entry<Node, Integer> adjacencyPair:
+				currentNode.getAdjacentNodes().entrySet()) {
+				Node adjacentNode = adjacencyPair.getKey();
+				Integer edgeWeight = adjacencyPair.getValue();
+				if (!settledNodes.contains(adjacentNode)) {
+					calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
+					unsettledNodes.add(adjacentNode);
+				}
+			}
+			settledNodes.add(currentNode);
+		}
+		return graph;
+	}
+
+	Node getLowestDistanceNode(Set < Node > unsettledNodes) {
+		Node lowestDistanceNode = null;
+		int lowestDistance = Integer.MAX_VALUE;
+		for (Node node: unsettledNodes) {
+			int nodeDistance = node.getDistance();
+			if (nodeDistance < lowestDistance) {
+				lowestDistance = nodeDistance;
+				lowestDistanceNode = node;
+			}
+		}
+		return lowestDistanceNode;
+	}
+
+	void calculateMinimumDistance(Node evaluationNode,
+								  Integer edgeWeigh, Node sourceNode) {
+		Integer sourceDistance = sourceNode.getDistance();
+		if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
+			evaluationNode.setDistance(sourceDistance + edgeWeigh);
+			LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+			shortestPath.add(sourceNode);
+			evaluationNode.setShortestPath(shortestPath);
+		}
+	}
+}
+
+class Graph {
+
+	private Set<Node> nodes = new HashSet<>();
+
+	public void addNode(Node nodeA) {
+		nodes.add(nodeA);
+	}
+
+	public void setNodes(Set<Node> nodes) {
+		this.nodes = nodes;
+	}
+
+	public Set<Node> getNodes() {
+		return this.nodes;
+	}
+}
+
+class Node {
+
+	private String name;
+
+	private List<Node> shortestPath = new LinkedList<>();
+
+	private Integer distance = Integer.MAX_VALUE;
+
+	Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+	public void addDestination(Node destination, int distance) {
+		adjacentNodes.put(destination, distance);
+	}
+
+	public Node(String name) {
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setShortestPath(List<Node> shortestPath) {
+		this.shortestPath = shortestPath;
+	}
+
+	public List<Node> getShortestPath() {
+		return this.shortestPath;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	public Integer getDistance() {
+		return this.distance;
+	}
+
+	public void setAdjacentNodes(Map<Node, Integer> adjacentNodes) {
+		this.adjacentNodes = adjacentNodes;
+	}
+
+	public Map<Node, Integer> getAdjacentNodes() {
+		return this.adjacentNodes;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return o instanceof Node && ((Node) o).name.equals(this.name);
+	}
+
+	@Override
+	public int hashCode() {
+		String[] splitStr = name.split(",");
+		return name.hashCode() + Integer.parseInt(splitStr[0]) + Integer.parseInt(splitStr[1]);
 	}
 }
